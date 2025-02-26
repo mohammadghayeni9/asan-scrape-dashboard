e4e5d58364a6f9aa7ecd3a7cdbd36663e68dee57
Mehdi <whoismdy@gmail.com>
2025-02-26 22:50:38 +0330

    haha

--- src/components/pages/robots/RobotsHistoryTabContent.vue
+++ src/components/pages/robots/RobotsHistoryTabContent.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-full h-full bg-border p-9 rounded-lg">
-    <div class="rounded-lg w-full h-full px-9 py-8 bg-secondary-300">ss</div>
+    <div class="rounded-lg w-full h-full px-9 py-8 bg-secondary-300">تاریخچه یافت نشد</div>
   </div>
 </template>
